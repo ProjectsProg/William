cccb8be60ed4657478aceca65b032f8346b6ccc4
ProjectsProg <projetomil300@gmail.com>
2025-04-15 15:11:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+# Instala o chromedriver compatível com o Chrome disponível
+chromedriver_autoinstaller.install()
+
 options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(options=options)
-driver.get("https://www.google.com")
-
-print("Título da página:", driver.title)
-
+driver.get("https://example.com")
+print(driver.title)
 driver.quit()
